072999e694fc2d7e49b4d453cf4301a94f3c766a
Phillip Alday <phillip.alday@mpi.nl>
2018-09-19 19:27:34 +0200

    model tuning

--- covariate.R
+++ covariate.R
@@ -1,3 +1,9 @@
+#' ---
+#' title: Statistical Analysis with Covariates for Musical Prediction Study
+#' author: Phillip M. Alday
+#' date: September 2018
+#' ---
+
 # Copyright 2018, Phillip Alday
 #
 # This program is free software: you can redistribute it and/or modify
@@ -18,7 +24,9 @@ library("tidyverse")  # for plotting and data manipulation
 library("lme4")
 library("car")
 library("effects")
+library("caret")
 library("lattice")
+library("corrplot")
 options(contrasts = c("contr.Sum","contr.Poly"))
 
 #' columns in this data file
@@ -86,45 +94,151 @@ dat <- dat %>%
 #' (1+condition|item).
 
 
+
+
 #' We also scaled the covariates. This helps the numerical aspects and also
 #' makes it easier to compare the relative weighting of the covariates.
 dat.scaled <- dat %>%
               mutate_at(vars(cloze:plaus_eval), scale)
 
+#' I'm not sure we should model both sentt_semdist and pt_semdist. They're
+#' closey related conceptually and strongly correlated numerically.
+
+cor.test(dat.scaled$sentt_semdist,dat.scaled$pt_semdist)
+
+#' So I'll leave one out (if you have theoretical reasons to prefer one to the
+#' other, feel free to change), which should also speed up model fitting!
+
+
+#' I was getting some interesting warnings about rank deficiency, which often
+#' happens when certain combinations don't exist / can be expressed as
+#' combinations of the other columns.
+
+
+X <- model.matrix(~ BS + condition * x * y * (cloze  + wordfrq + phon_nd + sentt_semdist + pt_semdist + concreteness + rhyme_eval + plaus_eval),data=dat.scaled)
+lcs <- findLinearCombos(X)
+colnames(X)[lcs$remove]
+
+#' Something weird is going on with condition B and the cloze probability
+#' manipulation. Is your manipulation purely cloze probability?
+#'
+#' Note also that there a bunch of NAs for cloze probability:
+
+dat %>% group_by(condition) %>%
+  summarize(mean=mean(cloze,na.rm=TRUE),
+            sd=sd(cloze,na.rm=TRUE),
+            NAs=sum(is.na(cloze)))
+#' All rows containing NAs in a predictor are dropped from the model. So maybe
+#' the better thing to do is omit cloze probability.
+#'
+#' Also, I think rhyme_eval is also an overspecification -- aren't the different
+#' conditions about different rhymes?
+#'
+#' If so, then you could model rhyme_eval *instead of* condition. The
+#' random-effects by item actually cover a lot of this type of small difference.
+#' This is why there's a huge literature on how important item-level random
+#' effects are in language research!
+
+
+# so that I don't have to remember the different options different optimizers have...
+bobyqa <- lmerControl(optimizer="bobyqa",optCtrl=list(maxfun=1e6))
+nlopt <- lmerControl(optimizer="nloptwrap",optCtrl=list(maxeval=1e6))
+
+#' I've also added in the third topographically predictor (z). This is a great
+#' example of issues in model fitting -- the simpler model without z didn't
+#' coverge, but the more complex one did.
+#'
+#' I've also removed the interactions between topography and covariates: they
+#' drastically increase fitting time, don't really improve fit, and greatly
+#' increase model complexity, so out they go!
+#'
+#' This gives us the power to try testing out interactions between condition,
+#' topography and the baseline interval. Adding in those interactions improves
+#' model fit by all measures (AIC, BIC, LRT) so they stay.
+#'
+#' I've also gone ahead and scaled both EEG measures (baseline and N4) -- this
+#' puts everything on the scale of standard deviations, so coefficients are
+#' essentially effect sizes in Cohen's $d$. The model fit is also much better:
+#' linear transformations such as scaling don't impact the model fit in terms of
+#' the pure mathematics, but they can make the optimizer's job easier. With this
+#' extra boost in fit, I was able to add in a by-condition slope for item.
+
+
 #+ model, cache=TRUE
-m <- lmer(N4 ~ BS + condition * x * y *
-            (cloze  + wordfrq + phon_nd + pt_semdist + sentt_semdist + concreteness + rhyme_eval + plaus_eval) +
+system.time(m <- lmer(scale(N4) ~ scale(BS) * condition * x * y * z +
+                              condition * (wordfrq + phon_nd + pt_semdist + concreteness + plaus_eval) +
             (1 + condition | subject_id) +
-            (1 | trlnumber),
+            (1 + condition | trlnumber),
           data=dat.scaled,
-          control=lmerControl(optimizer="bobyqa",optCtrl=list(maxfun=1e6)),
-          REML=FALSE)
+          control=bobyqa,
+          REML=FALSE))
+
 #+ output
-print(summary(m),correlation=TRUE,symbolic.cor=TRUE)
+print(summary(m),correlation=FALSE,symbolic.cor=TRUE)
+
+sprintf("Number of fixed-effect correlations > 0.1: %d",sum(as.matrix(vcov(m)) > 0.1))
 
 plot(m)
 
 qqmath(m)
 
-Anova(m)
+(a <- Anova(m))
+
+#' For the ANOVA-style display, we can also omit the baseline interval because
+#' we don't actually care about it, even if we have to model it.
+
+a[!str_detect(rownames(a),"BS"),]
+
 
 #' The above summary can also be expressed graphically.
 
 # Wald is the fastest method; boot the most accurate and slowest, profile is a
 # the middle road
 ci <- confint(m,method="Wald")
-ci %>%
+
+#+ coefplot, cache=TRUE, fig.width=7, fig.height=13
+ci.gg <- ci %>%
   as_tibble(rownames = "coef") %>%
-  filter(substr(coef,1,4) != ".sig") %>% # omit random effects
+  dplyr::filter(substr(coef,1,4) != ".sig") %>% # omit random effects
   mutate(est = fixef(m),
          coef = factor(coef, levels=rev(names(fixef(m))))) %>%
-  ggplot(aes(x=coef,y=est,ymin=`2.5 %`, ymax=`97.5 %`)) +
+  dplyr::filter(coef != "(Intercept)") %>% # intercept
+  dplyr::filter(!str_detect(coef,"BS")) %>% # baseline terms
+  mutate(topo=as.logical(str_detect(coef,"x|y|z")))
+
+
+gg <- ggplot(mapping=aes(x=coef,y=est,ymin=`2.5 %`, ymax=`97.5 %`)) +
   geom_pointrange(fatten=1.5) +
   geom_hline(yintercept=0, linetype="dashed") +
-  labs(x="Coefficient",y="Estimate (µV)") +
+  labs(x="Coefficient",y="Estimate (standardized difference)") +
   coord_flip() +
   theme_light()
 
-#' For now, I'm leaving out ways of plotting the statistical model, but that's
-#' something we can add later. Including potentially ways to plot the
-#' coefficients as ERP topographies.
+gg %+% subset(ci.gg, !topo) + ggtitle("Global Effects")
+
+gg %+% subset(ci.gg, as.logical(str_detect(coef,"x"))) + ggtitle("Lateral Effects")
+gg %+% subset(ci.gg, as.logical(str_detect(coef,"y"))) + ggtitle("Saggital Effects")
+
+#' With effects plots by quadrant
+#+ effects, cache=TRUE
+system.time(e <- allEffects(m, KR=FALSE))
+
+#+ effects-plot, cache=TRUE, fig.width=7, fig.height=13
+
+# skip the final x-y stuff
+plot(e[1:5],rug=FALSE,multiline=TRUE,ci.style="band")
+
+# e.con <- as.data.frame(e[["condition:x:y:z:concreteness"]])
+# e.con %>% group_by(condition,concreteness,x,y) %>%
+#   summarise_all(mean) %>%
+#   ggplot(aes(x=x,y=y,z=fit,fill=fit)) +
+#   geom_contour() +
+#   geom_raster(alpha=0.1) +
+#   facet_grid(concreteness ~ condition) +
+#   scale_fill_distiller(type="div") +
+#   theme_light()
+
+#' # Session Info
+#'
+
+sessionInfo()
